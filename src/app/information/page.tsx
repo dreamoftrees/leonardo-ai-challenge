add3bd96f383c8998b3ecf44be587ba2d2b88dd1
Charlie McKenzie <charlie@dreamoftrees.com>
2026-01-08 15:43:28 +1100

    fix: minor index fix

--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -19,7 +19,7 @@ function CharacterGridSkeleton() {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {Array.from({ length: 20 }).map((_, i) => (
-        <div key={i} className="space-y-2">
+        <div key={`skeleton-${i}`} className="space-y-2">
           <Skeleton className="aspect-square w-full rounded-xl" />
           <Skeleton className="h-4 w-3/4" />
           <Skeleton className="h-3 w-1/2" />
